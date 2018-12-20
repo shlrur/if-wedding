5a494f5215bff915da4f5d617d45434e195b9654
shlrur <shlrur123@gmail.com>
2018-12-20 23:42:15 +0900

    ENH
    add widgetContainer component

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,47 +1,31 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-
-import {
-    getWidgetTypesRequest,
-    getUseWidgetsRequest
-} from '../redux/actions/widgets';
+// import { connect } from 'react-redux';
 
 import Authentication from './authentication';
+import WidgetContainer from './widgetContainer';
 
-class Dashboard extends Component {
+export default class Dashboard extends Component {
     render() {
         return (
             <div>
                 <Authentication />
-                <button onClick={this.getWidgetTypes.bind(this)}>
-                    get widget types
-                </button>
-                <button onClick={this.getUsingWidgets.bind(this)}>
-                    get in use widgets
-                </button>
+                <WidgetContainer />
+                
             </div>
         );
     }
-
-    getWidgetTypes() {
-        this.props.getWidgetTypesRequest();
-    }
-
-    getUsingWidgets() {
-        this.props.getUseWidgetsRequest();
-    }
 }
 
-const mapStateToProps = state => ({
-    widgetTypes: state.widget.widgetTypes,
-    usingWidgets: state.widget.usingWidgets
-})
-const mapDispatchToProps = {
-    getWidgetTypesRequest,
-    getUseWidgetsRequest
-}
+// const mapStateToProps = state => ({
+//     widgetTypes: state.widget.widgetTypes,
+//     usingWidgets: state.widget.usingWidgets
+// })
+// const mapDispatchToProps = {
+//     getWidgetTypesRequest,
+//     getUseWidgetsRequest
+// }
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(Dashboard)
+// export default connect(
+//     mapStateToProps,
+//     mapDispatchToProps
+// )(Dashboard)
