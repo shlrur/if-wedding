609efa9a3a6737bff5edf25c4fae2161c3702c60
shlrur <shlrur123@gmail.com>
2019-01-19 23:18:39 +0900

    ENH
    creating some pages

---
+++ src/components/authorized/coupleInform.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class CoupleInformation extends Component {
+    render() {
+        return (
+            <div>
+                CoupleInformation
+            </div>
+        );
+    }
+}
