8ca4668233468d2e4f9c906c0723289cce0870ad
shlrur <shlrur123@gmail.com>
2019-02-11 23:26:14 +0900

    ENH
    implementing map in wedding information

--- src/components/authorized/weddingInform.js
+++ src/components/authorized/weddingInform.js
@@ -4,6 +4,13 @@ import { connect } from 'react-redux';
 import { setWeddingInformation } from '../../redux/actions/authentication';
 
 class WeddingInformation extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            searchKeyword: ''
+        };
+    }
     render() {
         return (
             <div className="wedding-information container">
@@ -29,8 +36,8 @@ class WeddingInformation extends Component {
                             <input type="text" className="form-control" id="bride-father-name" placeholder="아버님" />
                         </div>
                         <div className="form-group">
-                            <label htmlFor="bride-mother-name">아버님 성함</label>
-                            <input type="text" className="form-control" id="bride-mother-name" placeholder="아버님" />
+                            <label htmlFor="bride-mother-name">어머님 성함</label>
+                            <input type="text" className="form-control" id="bride-mother-name" placeholder="어머님" />
                         </div>
                         <div className="form-group">
                             <label htmlFor="bride-phone-number">전화번호</label>
@@ -58,8 +65,8 @@ class WeddingInformation extends Component {
                             <input type="text" className="form-control" id="groom-father-name" placeholder="아버님" />
                         </div>
                         <div className="form-group">
-                            <label htmlFor="groom-mother-name">아버님 성함</label>
-                            <input type="text" className="form-control" id="groom-mother-name" placeholder="아버님" />
+                            <label htmlFor="groom-mother-name">어머님 성함</label>
+                            <input type="text" className="form-control" id="groom-mother-name" placeholder="어머님" />
                         </div>
                         <div className="form-group">
                             <label htmlFor="groom-phone-number">전화번호</label>
@@ -70,7 +77,7 @@ class WeddingInformation extends Component {
                 <hr />
                 <div className="text-center">
                     <h2>인사말</h2>
-                    <textarea className="form-control" rows="3"></textarea>
+                    <textarea className="form-control" rows="3" placeholder="ex) 부디 참석하시어 많은 축복과 격려해 주십시오."></textarea>
                 </div>
                 <hr />
                 <div className="text-center">
@@ -78,7 +85,12 @@ class WeddingInformation extends Component {
                     <div className="row wedding-hall-map">
                         <div id="map" />
                         <div id="map-search">
-                            asdasd
+                            <div className="search-keyword">
+                                <input type="text" value={this.state.searchKeyword} onChange={this.searchKeywordChange.bind(this)} />
+                                <button onClick={this.placeSearch.bind(this)}>찾기</button>
+                            </div>
+                            <ul>
+                            </ul>
                         </div>
                     </div>
                 </div>
@@ -88,10 +100,58 @@ class WeddingInformation extends Component {
 
     componentDidMount() {
         const el = document.getElementById('map');
-        let daumMap = new window.daum.maps.Map(el, {
-            center: new window.daum.maps.LatLng(33.450701, 126.570667),
+        this.map = new window.daum.maps.Map(el, {
+            center: new window.daum.maps.LatLng(33.450701, 126.570667)
+        });
+
+        this.ps = new window.daum.maps.services.Places();
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                this.map.setCenter(new window.daum.maps.LatLng(position.coords.latitude, position.coords.longitude));
+            });
+        }
+    }
+
+    placeSearch() {
+        console.log(this.state.searchKeyword);
+        if (!this.state.searchKeyword.replace(/^\s+|\s+$/g, '')) {
+            console.log('no keyword');
+
+            return;
+        }
+
+        this.ps.keywordSearch(this.state.searchKeyword, this.placesSearchCB);
+    }
+
+    searchKeywordChange(e) {
+        this.setState({
+            searchKeyword: e.target.value
         });
     }
+
+    placesSearchCB(data, status, pagination) {
+        if (status === window.daum.maps.services.Status.OK) {
+            console.log('searched');
+            // 정상적으로 검색이 완료됐으면
+            // 검색 목록과 마커를 표출합니다
+            //displayPlaces(data);
+            // 페이지 번호를 표출합니다
+            //displayPagination(pagination);
+
+        } else if (status === window.daum.maps.services.Status.ZERO_RESULT) {
+
+            console.log('검색 결과가 존재하지 않습니다.');
+            return;
+
+        } else if (status === window.daum.maps.services.Status.ERROR) {
+
+            console.log('검색 결과 중 오류가 발생했습니다.');
+            return;
+
+        }
+    }
+
 }
 
 const mapStateToProps = state => ({
