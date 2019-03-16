0b09656bf4d0674f35b49474c038c02ea678b3f1
shlrur <shlrur123@gmail.com>
2019-03-16 23:52:36 +0900

    #10
    ENH
    creating&modifying widget component files for preview.

--- src/components/widgets/edit/bright/greeting1.js
+++ src/components/widgets/edit/bright/greeting1.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 
 import i18n from '../../../../i18n/i18n';
+
+import { setGeneralWidgetConfigsRequest } from '../../../../redux/actions/widgetConfig';
 
 class BrightGreeting1Edit extends Component {
     constructor(props) {
@@ -30,32 +33,57 @@ class BrightGreeting1Edit extends Component {
             groomParents,
             greetingText: widgetConfig.greetingText || defaultWeddingInform.greetingText
         };
+
+        this.debounced = _.debounce(this.setConfigHandler.bind(this), 1000);
+        this.objectForPreventDuplicated = {
+            brideName,
+            brideParents,
+            groomName,
+            groomParents,
+            greetingText: widgetConfig.greetingText || defaultWeddingInform.greetingText
+        };
     }
 
     render() {
         return (
             <div className="widget-greeting-bright-1">
                 <div id="groom">
-                    <p id="groom-name">
-                        {this.state.groomName}
-                    </p>
-                    <p id="groom-parents">
-                        {this.state.groomParents}
-                    </p>
+                    <input type="text" className="form-control" id="groom-name" placeholder={i18n.t('widgets.greeting_1.placeholder.groomName')}
+                        value={this.state.groomName}
+                        onChange={this.onChangeHandler.bind(this, 'groomName')} />
+                    <input type="text" className="form-control" id="groom-parents" placeholder={i18n.t('widgets.greeting_1.placeholder.groomParents')}
+                        value={this.state.groomParents}
+                        onChange={this.onChangeHandler.bind(this, 'groomParents')} />
                 </div>
                 <div id="bride">
-                    <p id="bride-name">
-                        {this.state.brideName}
-                    </p>
-                    <p id="bride-parents">
-                        {this.state.brideParents}
-                    </p>
+                    <input type="text" className="form-control" id="bride-name" placeholder={i18n.t('widgets.greeting_1.placeholder.brideName')}
+                        value={this.state.brideName}
+                        onChange={this.onChangeHandler.bind(this, 'brideName')} />
+                    <input type="text" className="form-control" id="bride-parents" placeholder={i18n.t('widgets.greeting_1.placeholder.brideParents')}
+                        value={this.state.brideParents}
+                        onChange={this.onChangeHandler.bind(this, 'brideParents')} />
                 </div>
-                <p id="greeting-text">
-                    {this.state.greetingText}
-                </p>
+                <input type="text" className="form-control" id="greeting-text" placeholder={i18n.t('widgets.greeting_1.placeholder.greetingText')}
+                    value={this.state.greetingText}
+                    onChange={this.onChangeHandler.bind(this, 'greetingText')} />
             </div>
         );
+    }
+
+    onChangeHandler(targetKey, e) {
+        this.setState({ [targetKey]: e.target.value });
+
+        this.debounced(targetKey);
+    }
+
+    setConfigHandler(targetKey) {
+        if (this.objectForPreventDuplicated[targetKey] === this.state[targetKey]) {
+            return;
+        }
+        
+        this.objectForPreventDuplicated[targetKey] = this.state[targetKey];
+
+        this.props.setGeneralWidgetConfigsRequest({ [targetKey]: this.state[targetKey] }, this.props.inform.id);
     }
 }
 
@@ -63,6 +91,7 @@ const mapStateToProps = state => ({
     user: state.authentication.user
 });
 const mapDispatchToProps = {
+    setGeneralWidgetConfigsRequest
 };
 
 export default connect(
